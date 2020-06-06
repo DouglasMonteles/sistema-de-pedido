6c5faf34ae33ed401e330bc7f7cda54fd52fea30
DouglasMonteles <silvadouglas328@gmail.com>
2020-06-06 12:49:37 -0300

    Endpoint: listar categorias /categorias

--- backend/src/main/java/com/douglas/backend/recourse/CategoriaRecourse.java
+++ backend/src/main/java/com/douglas/backend/recourse/CategoriaRecourse.java
@@ -1,16 +1,28 @@
 package com.douglas.backend.recourse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.douglas.backend.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaRecourse {
 
 	@GetMapping
-	public String listar() {
-		return "Categorias";
+	public List<Categoria> findAll() {
+		Categoria c1 = new Categoria(1, "Informática");
+		Categoria c2 = new Categoria(2, "Cama, mesa e banho");
+		
+		List<Categoria> list = new ArrayList<Categoria>();
+		list.add(c1);
+		list.add(c2);
+		
+		return list;
 	}
 	
 }
