fad410d944a926dcc932a87e44085fc49aa71943
DouglasMonteles <silvadouglas328@gmail.com>
2020-06-06 12:22:04 -0300

    Criando o projeto

---
+++ backend/src/main/java/com/douglas/backend/recourse/CategoriaRecourse.java
@@ -0,0 +1,16 @@
+package com.douglas.backend.recourse;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaRecourse {
+
+	@GetMapping
+	public String listar() {
+		return "Categorias";
+	}
+	
+}
