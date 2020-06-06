372235dd20496ecfd44212a6470fbe0b17e23ceb
DouglasMonteles <silvadouglas328@gmail.com>
2020-06-06 18:24:58 -0300

    endpoint de deleção de categoria

--- backend/src/main/java/com/douglas/backend/recourse/CategoriaRecourse.java
+++ backend/src/main/java/com/douglas/backend/recourse/CategoriaRecourse.java
@@ -1,11 +1,18 @@
 package com.douglas.backend.recourse;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.douglas.backend.domain.Categoria;
 import com.douglas.backend.service.CategoriaService;
@@ -21,6 +28,29 @@ public class CategoriaRecourse {
 	public ResponseEntity<?> findOne(@PathVariable Integer id) {
 		Categoria obj = service.findOne(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId())
+				.toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Void> update(@PathVariable Integer id, @RequestBody Categoria obj) {
+		obj.setId(id);
+		service.update(obj);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> excluir(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 	
 }
--- backend/src/main/java/com/douglas/backend/service/CategoriaService.java
+++ backend/src/main/java/com/douglas/backend/service/CategoriaService.java
@@ -1,8 +1,10 @@
 package com.douglas.backend.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.douglas.backend.domain.Categoria;
@@ -17,6 +19,35 @@ public class CategoriaService {
 	public Categoria findOne(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
 		return obj.orElseThrow(null);
+	}
+	
+	public List<Categoria> findAll() {
+		List<Categoria> list = repository.findAll();
+		return list;
+	}
+	
+	public Categoria insert(Categoria obj) {
+		obj = repository.save(obj);
+		return obj;
+	}
+	
+	public Categoria update(Categoria obj) {
+		Categoria newObj = findOne(obj.getId());
+		updateData(obj, newObj);
+		return repository.save(newObj);
+	}
+	
+	public void updateData(Categoria obj, Categoria newObj) {
+		newObj.setNome(obj.getNome());
+	}
+	
+	public void delete(Integer id) {
+		findOne(id);
+		try {
+			repository.deleteById(id);
+		} catch (Exception e) {
+			throw new DataIntegrityViolationException("Nao é posível excluir uma categoria que possui produtos vinculados a ela.");
+		}
 	}
 	
 }
